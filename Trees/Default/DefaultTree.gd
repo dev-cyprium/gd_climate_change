e84d6644c471485b177b0f4605c0e7acb519da63
Stefan Kupresak <stefan_vg@hotmail.com>
2020-07-23 20:52:27 +0200

    tweeks

--- Trees/Default/DefaultTree.gd
+++ Trees/Default/DefaultTree.gd
@@ -1,10 +1,8 @@
 extends KinematicBody2D
 
-
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("fader"):
 		$Animator.play("Fade")
-
 
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("fader"):
